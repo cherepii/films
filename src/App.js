2e6d2637edc966e2032fd2fc0244002f3e42686f
mark <mcherepivskiy04@gmail.com>
2022-12-16 23:23:44 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [films, setFilms] = useState(null)
+
+  const getFilms = async() => {
+    const res = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top', {
+      method: 'GET',
+      headers: {
+          'X-API-KEY': '051b0115-c788-493f-8f1e-bff66e70950d',
+          'Content-Type': 'application/json',
+      },
+    })
+
+    const films = await res.json()
+    console.log(films)
+
+    setFilms(films)
+  }
+
+  useEffect(() => {
+    getFilms()
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='wrapper'>
+          <h1>Список фильмов взят из неофициальной базы данных KinoPoisk</h1>
+        
+          {films && !!films.films.length && films.films.map(item => (
+            <div className='film__item' key={item.filmId}>
+              <p>{item.nameRu} / {item.rating} / {item.year} года выпуска / продолжительностью {item.filmLength}</p>
+              <img src={item.posterUrlPreview} alt="poster preview" />
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
